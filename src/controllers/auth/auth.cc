2c9b304553dce7494d5f4fc481132dd88e85d370
opessoa <omar.pessoa@gmail.com>
2022-12-08 14:44:51 -0300

    errors

--- src/controllers/auth/auth.cc
+++ src/controllers/auth/auth.cc
@@ -15,7 +15,7 @@ void auth::getToken(const HttpRequestPtr &request, std::function<void(const Http
     auto jwtGenerated = JWT::generateToken();/*ex3,
         responseJson.isMember("remember") && responseJson["remember"].asBool()
     );*/
-    std::int64_t jwtExpiration = jwtGenerated.getExpiration();
+    Json::Value::Int64 jwtExpiration = jwtGenerated.getExpiration();
 
     resultJson["token"] = jwtGenerated.getToken();
     resultJson["expiresIn"] = jwtExpiration - std::chrono::duration_cast<std::chrono::seconds>(std::chrono::system_clock::now().time_since_epoch()).count();
@@ -28,14 +28,14 @@ void auth::getToken(const HttpRequestPtr &request, std::function<void(const Http
 void auth::verifyToken(const HttpRequestPtr &request, std::function<void(const HttpResponsePtr &)> &&callback) {
     Json::Value resultJson;
 
-    resultJson["aud"] = request->getAttributes()->get<std::string>("jwt_aud");
-    resultJson["exp"] = request->getAttributes()->get<std::int64_t>("jwt_exp");
-    resultJson["iat"] = request->getAttributes()->get<std::int64_t>("jwt_iat");
+   /* resultJson["aud"] = request->getAttributes()->get<std::string>("jwt_aud");
+    resultJson["exp"] = request->getAttributes()->get<Json::Value::Int64>("jwt_exp");
+    resultJson["iat"] = request->getAttributes()->get<Json::Value::Int64>("jwt_iat");
     resultJson["iss"] = request->getAttributes()->get<std::string>("jwt_iss");
-    resultJson["nbf"] = request->getAttributes()->get<std::int64_t>("jwt_nbf");
+    resultJson["nbf"] = request->getAttributes()->get<Json::Value::Int64>("jwt_nbf");
     resultJson["email"] = request->getAttributes()->get<std::string>("jwt_email");
     resultJson["jwt_debugger"] = "https://jwt.io/#debugger-io?token=" + request->getHeader("Authorization").substr(7);
     resultJson["status"] = 1;
-
+*/
     return callback(HttpResponse::newHttpJsonResponse(resultJson));
 }
